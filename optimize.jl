c56e7e5c9d3c5083891272ce97d857cf15898d1e
ckaijun0 <ckaijun@stanford.edu>
2023-06-01 20:13:32 -0700

    updated optimize.jl

--- optimize.jl
+++ optimize.jl
@@ -24,6 +24,8 @@ function optimize(X,U)
     return X,U,T
 end
 
+#######################################################################################################################################
+
 # Specify the centerline track coordinates here (don't duplicate start and end points)
 x_points = [1; 2; 3; 3; 2; 1; 0; 0]
 y_points = [0; 0; 1; 2; 3; 3; 2; 1]
@@ -33,7 +35,7 @@ width = 0.1
 # Parameters for user input
 vmax = 50 # m/s (111.8mph)
 amax = 3 # m/s^2
-θmax = 45 # degrees 
+θmax = 90 # degrees 
 ωmax = 15 # ω=θ/t (assuming full lock in 3seconds)
 m = 1500 # kg
 friction_coefficient = 0.7 # 0.7=dry road, 0.4=wet road
@@ -46,40 +48,61 @@ Ac = Fc/m # centripetal acceleration
 
 # State variables vector
 S = ones(5, N)
-U = ones(2, N)
+U = zeros(2, N)
+U[1,:] = [pi/4 for _ = 1:9]
 
 track_bound = create_track_width(center_line, width)
-# S[:,1] = track_bound[3][1]
+S[1,:] = track_bound[3][1]
+S[2,:] = track_bound[3][2]
+S[4,:] = zeros(N)
 
+# Create the semi holonomic model, to calculate S+1 from S and U
+function semi_holonomic_model(S, U, local_track_bound)
+    S_next = copy(S)
+    point_R, point_L = local_track_bound[1], local_track_bound[2]
+    d = get_distance(S[1:2], point_R, point_L, S[4])
+    S_next[3] = sqrt(S[3]^2 + 2*U[2]*d)
+    if U[2] == 0
+        S_next[5] = d/S[3]
+    else
+        S_next[5] = (S_next[3]-S[3])/U[2]
+    end
+    S_next[1] = S[1] + d*cos(S[4])
+    S_next[2] = S[2] + d*sin(S[4])
+    S_next[4] = S[4] + U[1]
+    return S_next
+end 
 
 # Compute State vector, S, given S[1] and Input vector, U
-function compute_state(initial_point, S, U)
-    S[:,1] = [initial_point; 0; 0; 0] # Give initial point, 
+function compute_state(S, U, track_bound)
     N = lastindex(S[1,:])
-    for n = 1:N
-        S = [semi_holonomic_model(S[n],U[n]) for n in 1:(N-1)]
+    for n = 1:(N-1)
+        local_track_bound = [[track_bound[1][1][n+1];track_bound[1][2][n+1]],[track_bound[2][1][n+1];track_bound[2][2][n+1]]]
+        S[:,n+1] = semi_holonomic_model(S[:,n],U[:,n], local_track_bound)
     end
     return S
 end
 
-# Create the semi holonomic model, to calculate S+1 from S and U
-function semi_holonomic_model(S,U)
-    S_next = copy(S)
-    S_next[1] = S[1] + (S[3]*S[5]+0.5*U[2]*S[5].^2).*cos(U[1])
-    S_next[2] = S[2] + (S[3]*S[5]+0.5*U[2]*S[5].^2).*sin(U[1])
-    S_next[3] = S[3] + U[2].*S[5]
-    S_next[4] = S[4] + U(1)
-    d = sqrt((S_next[1]-S[1]).^2 + (S_next[2]-S[2]).^2)
-    S_next[5] = max((-S(3)+sqrt(S[3].^2-2*U[2]*d))/U[2],(-S(3)-sqrt(S[3].^2-2*U[2]*d))/U[2])
-    return S_next
-end 
+# Get distance from point
+function get_distance(point,point_R,point_L,θ)
+    x0, y0 = point[1], point[2]
+    xR, yR = point_R[1], point_R[2]
+    xL, yL = point_L[1], point_L[2]
+    m = (yR-yL)/(xR-xL)
+    c = -m*xR+yR
+    d = (m*x0-y0+c)/(sin(θ)-m*cos(θ))
+    return d
+end
 
-# Objective: Total time
-function total_time(S)
+# Compute total time
+function compute_total_time(S)
     return sum(S[5,:])
 end
 
-# Constraints
+# Initial design point
+x0 = U
+
+# Constraint function (c)
 function constraints(S, U, track_bound; ϵ=1e-5)
     coord = S[1:2,:]
     bound1, bound2 = reduce(hcat,track_bound[1])', reduce(hcat,track_bound[2])' # Convert vector of vector to array
@@ -92,14 +115,21 @@ function constraints(S, U, track_bound; ϵ=1e-5)
     constrain_vector = [all_distances.-ϵ;
                         v./vmax.-1;
                         a./amax.-1;
-                        θ./θmax.-1;
-                        ω./ωmax.-1;
+                        abs.(θ)./θmax.-1;
+                        abs.(ω)./ωmax.-1;
                         v.*ω./(Fc*m).-1]
     return constrain_vector
 end
 
-c = constraints(S, U, track_bound; ϵ=1e-5)
-# print(c)
+# Objective function (f)
+function objective_function(U)
+    design_point = compute_state(S, U, track_bound)
+    total_time = compute_total_time(design_point)
+    return total_time
+end
+
+#######################################################################################################################################
+
 
 using LinearAlgebra
 using Plots
